464d98f286dd5f3a3cf53657ea27e0a0a065d753
Logan Wood <lwood3499@gmail.com>
2018-04-24 22:36:09 -0500

    added tictactoe

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 import Simon from '../src/components/Simon';
+import TicTacToe from '../src/components/TicTacToe';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Simon />
+        <div className="simon">
+          <Simon />
+        </div>
+        <div className="tictactoe">
+          <TicTacToe />
+        </div>
       </div>
     );
   }
